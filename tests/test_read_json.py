0566d56c45f4569b4db9707776c51daec9eaf5a3
Larisa <larissaatmasova@mail.ru>
2024-11-21 12:55:55 +0500

    Домашнее задание по разделу 14.1

--- tests/test_read_json.py
+++ tests/test_read_json.py
@@ -1,7 +1,5 @@
-import json
 import os
 import pytest
-from json import JSONDecodeError
 
 from src.read_json import read_json
 
@@ -18,7 +16,6 @@ def path_empty_list() -> str:
     return PATH_TO_FILE
 
 
-
 def test_read_json_no_file() -> None:
     assert read_json("no_file") == []
 
@@ -29,26 +26,20 @@ def test_read_json_empty_list(path_empty_list: str) -> None:
 
 def test_read_json(path: str) -> None:
     assert read_json(path)[0] == {
-    "name": "Смартфоны",
-    "description": "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни",
-    "products": [
-        {
-            "name": "Samsung Galaxy C23 Ultra",
-            "description": "256GB, Серый цвет, 200MP камера",
-            "price": 180000.0,
-            "quantity": 5
-        },
-        {
-            "name": "Iphone 15",
-            "description": "512GB, Gray space",
-            "price": 210000.0,
-            "quantity": 8
-        },
-        {
-            "name": "Xiaomi Redmi Note 11",
-            "description": "1024GB, Синий",
-            "price": 31000.0,
-            "quantity": 14
-        }
-    ]
-}
+        "name": "Смартфоны",
+        "description": "Смартфоны, как средство не только коммуникации, "
+                       "но и получение дополнительных функций для удобства жизни",
+        "products": [
+            {"name": "Samsung Galaxy C23 Ultra",
+             "description": "256GB, Серый цвет, 200MP камера",
+             "price": 180000.0,
+             "quantity": 5},
+            {"name": "Iphone 15",
+             "description": "512GB, Gray space",
+             "price": 210000.0,
+             "quantity": 8},
+            {"name": "Xiaomi Redmi Note 11",
+             "description": "1024GB, Синий",
+             "price": 31000.0, "quantity": 14}
+        ]
+    }
